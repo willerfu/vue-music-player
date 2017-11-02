fc49e2fcd97a12c8cb009d632a1327b88702177b
willerfu <56144678@qq.com>
2017-11-02 15:16:12 +0800

    修改音乐外链

--- mock/musiclist.js
+++ mock/musiclist.js
@@ -3,19 +3,19 @@ export const MUSIC_LIST = [
     id: 1,
     title: '第三类接触',
     artist: '潘玮柏',
-    file: 'http://m10.music.126.net/20171101234559/c57f6003c526817c98b3c91aaa942ebc/ymusic/a34f/4478/2216/73ff6aa31fab16292ef3848089930d70.mp3',
+    file: 'http://url.527578.com/kuwo.php/27169063.mp3',
     cover: 'http://p1.music.126.net/K7gf0twm2Blr3toYTYpvkw==/18346451021828060.jpg?param=300y300'
   }, {
     id: 2,
     title: 'JULY',
     artist: '吴亦凡',
-    file: 'http://m10.music.126.net/20171101233214/f08741ce48c02fd46f973eb731a8335b/ymusic/d916/b80d/dc1f/ce3051f11cd2c8de841434b772415829.mp3',
+    file: 'http://url.527578.com/kuwo.php/9937206.mp3',
     cover: 'http://p1.music.126.net/EbtEQchXFKPOc1dQbxM6ig==/18550960184779345.jpg?param=300y300'
   }, {
     id: 3,
     title: '童话镇',
     artist: '陈一发儿',
-    file: 'http://m10.music.126.net/20171101233245/1499be1cd2de0c75214f110b1046c856/ymusic/87b2/b6b6/2862/25c7104cecb596141c14328f7f949e05.mp3',
+    file: 'http://url.527578.com/kuwo.php/11425031.mp3',
     cover: 'http://p1.music.126.net/tfa811GLreJI_S0h9epqRA==/3394192426154346.jpg?param=300y300'
   }, {
     id: 4,
@@ -33,37 +33,37 @@ export const MUSIC_LIST = [
     id: 6,
     title: '刚好遇见你',
     artist: '李玉刚',
-    file: 'http://m10.music.126.net/20171101233148/0a35c9e81683e989391249f42745f018/ymusic/3466/f88c/165c/3520e5664afd420989e88bc3a694c237.mp3',
+    file: 'http://url.527578.com/kuwo.php/9950164.mp3',
     cover: 'http://p1.music.126.net/lDyytkTaPYVTb1Vpide6AA==/18591642115187138.jpg?param=300y300'
   },
   {
     id: 7,
     title: '我的新衣',
     artist: 'VaVa',
-    file: 'http://m10.music.126.net/20171101233413/33f2388f8eae768a2f1650fee915c402/ymusic/575a/393a/9bfc/fda7901e5fb958db1af4c7931f1490d1.mp3',
+    file: 'http://url.527578.com/kuwo.php/28501778.mp3',
     cover: 'http://p1.music.126.net/_eeZTnpBkkKX_oQ4LLbj7Q==/109951163045657251.jpg?param=300y300'
   },
   {
     id: 8,
     title: 'sixteen slang(十六slang)',
     artist: 'Gil G',
-    file: 'http://m10.music.126.net/20171101233820/a6c1920c2892ad2cf90db8b37906f29e/ymusic/e271/8bf2/3799/595a4e568bb0e98bf7d37e51e84de455.mp3',
+    file: 'http://url.527578.com/kuwo.php/28659489.mp3',
     cover: 'http://p1.music.126.net/6O5IY0maZa3NedTCjiBh2A==/109951162988770549.jpg?param=300y300'
   },
   {
     id: 9,
-    title: '悄悄离开北师珠',
-    artist: 'CAY',
-    file: 'http://m10.music.126.net/20171101233921/69ac37679227d8857b3a205fb7d94c00/ymusic/334f/720c/1497/1abe30aad837f59e38afee271dda95f0.mp3',
-    cover: 'http://p1.music.126.net/uU4-suGRXMPRhqsoD461AA==/109951162979252886.jpg?param=300y300'
-  },
-  {
-    id: 10,
     title: 'color-X 3D',
     artist: '徐梦圆',
-    file: 'http://m10.music.126.net/20171101234142/b7712d636b72048b48c379c24883457a/ymusic/dd1a/ab57/6654/048683ccdd8d276612f14c46f1745fd5.mp3',
+    file: 'http://url.527578.com/kuwo.php/6942048.mp3',
     cover: 'http://p1.music.126.net/EzP-01fkE9gzu9Tlj09d0A==/18315664695526776.jpg?param=300y300'
-  }
+  },
+  {
+		id: 10,
+		title: 'FXXK IT',
+		artist: 'bigbang',
+		file: 'http://www.170mv.com/kw/other.web.ri01.sycdn.kuwo.cn/resource/n1/96/84/1523189814.mp3',
+		cover: 'http://p4.music.126.net/YFz7JOGYtid5MoecLO6Otw==/3412884134806295.jpg?param=300y300'
+	}
 
 
 ];
